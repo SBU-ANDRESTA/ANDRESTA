011f22867f9d76ddfafee6fd5ef679bc22cb29eb
SBU-ANDRESTA <mahdi.abaszade@gmail.com>
2020-02-07 15:10:39 +0330

    Remove

--- ANDRESTA/packet_transaction_lib/buffer/ringbuffer.h
+++
@@ -1,2 +0,0 @@
-#define RINGBUFFER_INSTANCE(name, dev) extern int alt_no_storage
-#define RINGBUFFER_INIT(name, dev) while (0)
--- ANDRESTA/templateEngine/cp_mdl_gnr.py
+++
@@ -1,7 +0,0 @@
-import dzn_generator
-import mapping_result
-
-def gen():
-    dzn_generator.gen()
-    mapping_result.run_minizinc()
-    mapping_result.run()
